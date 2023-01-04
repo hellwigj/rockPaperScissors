519c6c50dd7d2e971225a8f696e0eabb033892d6
Jeremy Hellwig <120408040+hellwigj@users.noreply.github.com>
2023-01-04 14:08:24 -0600

    I forgot to commit until I finished the assignment

--- file.js
+++ file.js
@@ -8,8 +8,9 @@ function getComputerChoice() {
     return choices[getRandom()];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let computerSelection = getComputerChoice();
     if (
         playerSelection === 'Rock' && computerSelection === 'Paper' ||
         playerSelection === 'Paper' && computerSelection === 'Scissors' ||
@@ -17,10 +18,22 @@ function playRound(playerSelection, computerSelection) {
         {
             return `You Lose! ${computerSelection} beats ${playerSelection}`;
         } 
-    else if (playerSelection != 'Rock' || playerSelection != 'Paper' || playerSelection != 'Scissors') {
+    else if (playerSelection != 'Rock' && playerSelection != 'Paper' && playerSelection != 'Scissors') {
             return "Player did not enter a valid selection";
     }
     else {
             return `You Win! ${playerSelection} beats ${computerSelection}`;
         }
 }
+
+function game(x) {  
+    for(let i = 1; i <= x; i++) {
+        let userSel = prompt('Please enter Rock, Paper, or Scissors');
+        console.log(playRound(userSel));
+    }
+}
+
+game(5);
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
